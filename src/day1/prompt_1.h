c2f47a32870a4f885f40c9420916c6372c9b15e8
Jonathan Clow <Jonathan.Clow@ibm.com>
2019-12-28 14:50:59 -0500

    added py solutions to day 1, start of day 2; cpp start for day 1

---
+++ src/day1/prompt_1.h
@@ -0,0 +1,17 @@
+//
+// Created by Jonathan Clow on 12/26/19.
+//
+
+#ifndef ADVENTOFCODE2019_PROMPT_1_H
+#define ADVENTOFCODE2019_PROMPT_1_H
+
+#endif //ADVENTOFCODE2019_PROMPT_1_H
+
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int part1();
+// int part2(int* input);
+int main();
